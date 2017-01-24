2ef08345358c59a45e0c1a08537ab2ebe1e0f4ee
alexchri <alexchri@umich.edu>
2017-01-24 16:38:54 -0500

    additional empty files

---
+++ calc.cpp
@@ -0,0 +1,9 @@
+//
+//  calc.cpp
+//  Roman
+//
+//  Created by Alexandre Rochaix on 1/24/17.
+//  Copyright © 2017 Alexandre Rochaix. All rights reserved.
+//
+
+#include "calc.h"
---
+++ calc.h
@@ -0,0 +1,14 @@
+//
+//  calc.hpp
+//  Roman
+//
+//  Created by Alexandre Rochaix on 1/24/17.
+//  Copyright © 2017 Alexandre Rochaix. All rights reserved.
+//
+
+#ifndef calc_hpp
+#define calc_hpp
+
+#include <stdio.h>
+
+#endif /* calc_hpp */
