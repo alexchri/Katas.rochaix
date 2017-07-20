27c4f9f45d253ee4286ebe3940569cda2e3eab91
alexchri <alexchri@umich.edu>
2017-07-19 20:56:30 -0400

    First commit for Vending Machine Kata

---
+++ Vending Machine/VendingMachineKata/VendingMachineMainFile.h
@@ -0,0 +1,17 @@
+//
+//  VendingMachineMainFile.hpp
+//  VendingMachineKata
+//
+//  Created by Alexandre Rochaix on 7/19/17.
+//
+//
+
+#ifndef VendingMachineMainFile_hpp
+#define VendingMachineMainFile_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+#endif /* VendingMachineMainFile */
+
+//init files!
---
+++ Vending Machine/VendingMachineKata/VendingMachineTestSuite.cpp
@@ -0,0 +1,14 @@
+//
+//  VendingMachineTestSuite.cpp
+//  VendingMachineKata
+//
+//  Created by Alexandre Rochaix on 7/19/17.
+//
+//
+#include <iostream>
+
+int main() {
+    //init files!
+    
+    return 0;
+}
